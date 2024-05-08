178e72eb8f51fa671f7a58d63a4cb2276b20f33d
Alexander Orlov <eadenink@gmail.com>
2024-05-09 00:10:58 +0400

    Add createEvent route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/eadenink/go-events/models"
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
@@ -18,5 +20,27 @@ func main() {
 func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{
 		"events": models.GetEvents(),
+	})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	event.ID = len(models.GetEvents()) + 1
+	event.DateTime = time.Now()
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{
+		"event": event,
 	})
 }
